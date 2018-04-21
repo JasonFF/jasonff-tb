4b9ab71e48c4ee5475603493c5f50c1ae40421d1
JasonFF <925784658@qq.com>
2018-04-21 10:57:50 +0800

    add

--- attack.js
+++ attack.js
@@ -13,19 +13,21 @@
 // }, 100)
 
 const phantom = require('phantom');
+let index = 0
 setInterval(() => {
   (async function() {
     const instance = await phantom.create();
     const page = await instance.createPage();
     await page.on('onResourceRequested', function(requestData) {
-      console.info('Requesting', requestData.url);
+      // console.info('Requesting', requestData.url);
     });
     // let url = 'https://item.taobao.com/item.htm?spm=a230r.7195193.1997079397.8.qaVREc&id=567880147066&abbucket=3' // js
     let url = 'https://item.taobao.com/item.htm?spm=a1z10.1-c.w1004-17519367055.1.18c76aa2vV60PY&id=565125865250'
     const status = await page.open(url);
     const content = await page.property('content');
-    console.log(content);
-  
+    // console.log(content);
+    index++
+    console.log(index)
     await instance.exit();
   })();
 }, 20000)
